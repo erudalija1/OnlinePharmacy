e38a51a7c259b6cb8df34417fdf485cca1018a8b
Ema <emaruda@gmail.com>
2022-05-09 01:15:16 +0200

    Initial commit

---
+++ src/main/java/ba/unsa/etf/onlinepharmacy/OnlinePharmacyApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.onlinepharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePharmacyApplication.class, args);
+    }
+
+}
